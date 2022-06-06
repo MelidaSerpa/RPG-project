e8588f0e3a341b9aef857ab1cea97b6e23504c0f
MelidaSerpa <106879841+MelidaSerpa@users.noreply.github.com>
2022-06-05 23:09:15 -0500

    Values and lists

---
+++ RPG_code.py
@@ -0,0 +1,22 @@
+import math
+import random
+
+#Boss atributtes
+boss_attacks = ["Body blast","Acid blow","Laser hands","Sin purifier"]
+failed_attack = "Failed attack"
+failed_attack = random.randint(0,100)
+boss_life = 250
+
+failed_attack = "Failed attack"
+failed_attack =  random.radint(0,100)
+
+#Kermit atributtes
+kermit_attacks = ["Ak 47","Slap","Machine gun"]
+Kermit_life = 150
+especial_attack = random.randint(0,100)
+
+#3 life potions
+life_potion = 25
+
+especial_attack = "Sexy smirk" 
+especial_attack = random.radint(0,100)
