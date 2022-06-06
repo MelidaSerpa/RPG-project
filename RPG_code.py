6b3d17973661d46be959f69f54869d9a97c302c2
MelidaSerpa <106879841+MelidaSerpa@users.noreply.github.com>
2022-06-06 13:19:55 -0500

    Correction

--- RPG_code.py
+++ RPG_code.py
@@ -3,8 +3,6 @@ import random
 
 #Boss atributtes
 boss_attacks = ["Body blast","Acid blow","Laser hands","Sin purifier"]
-failed_attack = "Failed attack"
-failed_attack = random.randint(0,100)
 boss_life = 250
 
 failed_attack = "Failed attack"
@@ -18,5 +16,5 @@ especial_attack = random.randint(0,100)
 #3 life potions
 life_potion = 25
 
-especial_attack = "Sexy smirk" 
+especial_attack = "Sexy smirk"
 especial_attack = random.radint(0,100)
