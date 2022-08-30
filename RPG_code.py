b0ca55abf0190bbedf231d9be7087c2d9cd38577
MelidaSerpa <106879841+MelidaSerpa@users.noreply.github.com>
2022-08-30 11:23:00 -0500

    Index Code updated

--- RPG_code.py
+++ RPG_code.py
@@ -1,20 +1,60 @@
 import math
 import random
+from CharacterClass import CharacterClass
+
+kermit = CharacterClass("Kermit The Warrior", 150, 1)
+elephant = CharacterClass("Blading Elephant", 250, 1)
 
 #Boss atributtes
-boss_attacks = ["Body blast","Acid blow","Laser hands","Sin purifier"]
-boss_life = 250
 
-failed_attack = "Failed attack"
-failed_attack =  random.radint(0,100)
+#boss_life = 250
+
+#failed_attack = "Failed attack"
+#failed_attack =  random.radint(0,100)
 
 #Kermit atributtes
-kermit_attacks = ["Ak 47","Slap","Machine gun"]
-Kermit_life = 150
-especial_attack = random.randint(0,100)
+#kermit_attacks = ["Ak 47","Slap","Machine gun"]
+#Kermit_life = 150
+#especial_attack = random.randint(0,100)
 
 #3 life potions
-life_potion = 25
+#life_potion = 25
 
-especial_attack = "Sexy smirk"
-especial_attack = random.radint(0,100)
+#especial_attack = "Sexy smirk"
+#especial_attack = random.radint(0,100)
+
+print(elephant.bossAtt())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
